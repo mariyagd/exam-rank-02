5bbfbb9fa7ae76b67be0d908c2919f2c019b14e8
mariyagd <109855801+mariyagd@users.noreply.github.com>
2022-12-28 13:24:25 +0100

    ok

---
+++ Level2/alpha_mirror/alpha_mirror2.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int	main(int ac, char *av[])
+{
+	int	i;
+
+	i = 0;
+	if (ac != 2)
+		write(1, "\n", 1);
+	else
+	{
+		while (av[1][i] != '\0')
+		{
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+				av[1][i] = 'z' - av[1][i] + 'a';
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				av[1][i] = 'Z' - av[1][i] + 'A';
+			write (1, &av[1][i], 1);
+			i++;
+		}
+		write (1, "\n", 1);
+	}
+	return (0);
+}
+
+
